8b8734ebda3a8ae128e31db47e2b5f45ef1b04c5
TokyHenintsoa00 <tokyramanalinarivo@gmail.com>
2026-05-12 10:14:54 +0300

    setup projet gondorchic

---
+++ backend/src/main/java/itu/BackendGondorChic/BackendGondorChicApplication.java
@@ -0,0 +1,13 @@
+package itu.BackendGondorChic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGondorChicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGondorChicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/itu/BackendGondorChic/BackendGondorChicApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.BackendGondorChic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGondorChicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
