655d7e97b4f28684663cf0654a510972d02616aa
Jason Andriniaina RALIJAONA <jasonralijaona@gmail.com>
2026-05-19 14:55:01 +0300

    feat : add swagger doc

--- backend/src/main/java/itu/gondorchic/config/SecurityConfig.java
+++ backend/src/main/java/itu/gondorchic/config/SecurityConfig.java
@@ -47,6 +47,14 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/v3/api-docs",
+                        "/swagger-resources/**",
+                        "/webjars/**"
+                    ).permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
